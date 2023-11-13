0497c8c21c44061c16b729bc5bb948fa57d38042
Dylan <no-reply@noreply.com>
2023-11-13 00:30:51 -0700

    Add SQL debugging, and debugging flags for easier debug toggling.

--- app/config/services/db.php
+++ app/config/services/db.php
@@ -3,5 +3,17 @@
 $Container->setShared("db", function () {
     $connectionParams = require __DIR__ . "/../db.php";
     $conn = new Phalcon\Db\Adapter\Pdo\Mysql($connectionParams);
+
+    if(DEBUG_SQL)
+    {
+        $eventsManager = new \Phalcon\Events\Manager();
+        $eventsManager->attach(
+            "db:beforeQuery",
+            function($event, $connection) {
+                bdump($connection->getSQLStatement());
+            }
+        );
+        $conn->setEventsManager($eventsManager);
+    }
     return $conn;
 });
