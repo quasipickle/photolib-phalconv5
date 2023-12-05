ddbc36318d8da771fa2d35316d60f0b8f75e41e0
Dylan <no-reply@noreply.com>
2023-12-04 19:16:40 -0700

    Add actual allowed size if file upload is prevented due to size.

--- app/components/Image/Validator/UploadValidator.php
+++ app/components/Image/Validator/UploadValidator.php
@@ -3,6 +3,7 @@
 namespace Component\Image\Validator;
 
 use Component\Image\File\UploadedFile;
+use Helper\ViewHelper;
 
 /**
  * Validates an uploaded file
@@ -41,7 +42,15 @@ class UploadValidator extends \Component\Image\Validator
             case \UPLOAD_ERR_OK:
                 return true;
             case \UPLOAD_ERR_INI_SIZE:
-                throw new \RuntimeException("File is larger that PHP allows.");
+                $uploadIni = trim(ini_get("upload_max_filesize"));
+                // source: https://stackoverflow.com/a/19570313/251859
+                $uploadInt = intval($uploadIni) * ([ 'g'=> 1<<30, 'm' => 1<<20, 'k' => 1<<10 ][strtolower(substr($uploadIni,-1))] ?: 1);
+                $postIni = trim(ini_get("post_max_size"));
+                // source: https://stackoverflow.com/a/19570313/251859
+                $postInt = intval($postIni) * ([ 'g'=> 1<<30, 'm' => 1<<20, 'k' => 1<<10 ][strtolower(substr($uploadIni,-1))] ?: 1);
+                $int = min($uploadInt, $postInt);
+                $hr = ViewHelper::filesize($int);
+                throw new \RuntimeException("File is larger that PHP allows: $hr.");
             case \UPLOAD_ERR_FORM_SIZE:
                 throw new \RuntimeException("File is larger than the form allowed.");
             case \UPLOAD_ERR_PARTIAL:
